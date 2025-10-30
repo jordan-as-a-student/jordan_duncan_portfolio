d0cebf8038f882d43c20f7acfc5b5c9234666b1a
Jordan Duncan <jordan4duncan@gmail.com>
2025-10-30 13:29:25 -0500

    updated personal_pic and exerience.js

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,13 @@ function App() {
             />
             <div className={"space-y-4 leading-relaxed text-neutral-300"}> 
               <p>
-                Hi, I'm <span className={"font-semibold text-white"}>Jordan Duncan</span> Me likey computie
+                Hi, I'm <span className={"font-semibold text-white"}>Jordan Duncan</span> a passionate undergraduate student at the University of Central Missouri studying Computer Science and Cybersecurity. 
               </p>
               <p>
-                I enjoy mice and key
+                I love learning, creating, and communicating about: software development, web development, cybersecurity, AI, and technology in general.
+              </p>
+              <p>
+                Outside of computies💻, I enjoy working out, reading about exsistential philosophy, watching Elvis the movie, spending my money unwisely at concerts, and <strike>spending time with all of my friends</strike> making friends!
               </p>
               <div className={"flex gap-3 pt-2 justify-center"}>
                 <a 
@@ -57,7 +60,7 @@ function App() {
           </Section>
 
       {/*Experience*/}
-      <Section id={"experience"} title={"Work Experience"} subtitle={"My Work Experience"}>
+      <Section id={"experience"} title={"Work Experience"} subtitle={"I am just beginning to acquire career field experience, but the listed jobs below have built my soft skills"}>
               <div className={"space-y-4 text-left"}>
                 {experience.map((job)=>(
                     <ExperienceItem key={job.id} item={job}/>
