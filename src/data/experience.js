cc81ce6cfcc90a56e2ceef3c58c240c979a5e0f5
Jordan Duncan <jordan4duncan@gmail.com>
2025-10-30 13:49:11 -0500

    dates

--- src/data/experience.js
+++ src/data/experience.js
@@ -4,8 +4,8 @@ export default[
         role: "Computer Science University Tutor",
         company: "University of Central Missouri",
         location: "Warrensburg, MO",
-        startDate: "October 2025",
-        endDate: "Present",
+        start: "October 2025",
+        end: "Present",
         bullets: [
             "Assisted students in understanding computer science concepts and coursework.",
             "Furthered my understanding of computer science by teaching to others.",
@@ -17,8 +17,8 @@ export default[
         role: "Online Pickup and Dispenser",
         company: "Walmart",
         location: "Aurora, MO",
-        startDate: "June 2025",
-        endDate: "August 2025",
+        start: "June 2025",
+        end: "August 2025",
         bullets: [
             "Working with a big cooperation, I learned to adapt to a structured environment with high expectations and strict scheduling.",
             "Working in a busy public area, I practiced using patience and professionalism when"
@@ -30,8 +30,8 @@ export default[
         role: "Sandwich Artist",
         company: "Subway",
         location: "Joplin, MO",
-        startDate: "February 2025",
-        endDate: "May 2025",
+        start: "February 2025",
+        end: "May 2025",
         bullets: [
             "Taught me how to balance work, school, and other time-consuming aspects of my life.",
             "I learned how to effectively manage challenging customers and efficiently take orders under pressure.",
@@ -42,8 +42,8 @@ export default[
         role: "Lifeguard",
         company: "YMCA",
         location: "Monett, MO",
-        startDate: "May 2024",
-        endDate: "August 2024",
+        start: "May 2024",
+        end: "August 2024",
         bullets: [
             "Worked together as a close-knit team with my fellow members of the aquatic staff.",
             "Dealt with patrons who were upset or angry, and grew my communication and leadership skills by taking control in these situations.",
@@ -54,8 +54,8 @@ export default[
         role: "Kitchen Staff",
         company: "Travel Centers of America (TA)",
         location: "Mount Vernon, MO",
-        startDate: "June 2023",
-        endDate: "August 2023",
+        start: "June 2023",
+        end: "August 2023",
         bullets: [
             "I learned how to perfect my methods of work through managerial constructive criticism.",
             "Strengthened my teamwork skills by learning how to function with staff of diverse ages and abilities."
@@ -66,8 +66,8 @@ export default[
         role: "Fry Cook",
         company: "Sonic",
         location: "Mount Vernon, MO",
-        startDate: "June 2022",
-        endDate: "August 2022",
+        start: "June 2022",
+        end: "August 2022",
         bullets: [
             "Self-assigned work and delegated tasks to other workers.",
             "Worked in tight, stressful, time-constrained situations to make product for demanding customers."
